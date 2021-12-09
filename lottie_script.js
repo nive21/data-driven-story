2e7fada076d1a123f0d55d968e0ebd16c4617287
Nivedhitha <nive.mathan21@gmail.com>
2021-12-08 21:21:41 -0500

    first commit

---
+++ lottie_script.js
@@ -0,0 +1,88 @@
+/*
+ * Libraries used:
+ * http://airbnb.io/lottie/#/web
+ * http://scrollmagic.io/docs/
+*/
+
+/* Get all Lottie animations on page */
+const animations = document.querySelectorAll('.lottie');
+
+/* Initialize ScrollMagic */
+const controller = new ScrollMagic.Controller();
+
+/* Get viewport height */
+var vh = $(window).height();
+
+/* Initialize each lottie animation */
+animations.forEach(async (animation) => {
+  const lottieInstance = lottie.loadAnimation({
+    container: animation,
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: animation.dataset.animPath
+  })
+
+
+  const animDuration = (() => {
+    let duration;
+
+    if (animation.dataset.animDuration) {
+      duration = animation.dataset.animDuration;
+      const durationIsNumString = /^\d+$/.test(duration);
+      if ( durationIsNumString ) {
+        duration = parseInt(duration)
+      }
+    } else {
+      duration = parentHeight;
+    }
+    return duration
+  })();
+
+  const animTriggerHook = (() => {
+    let triggerHook;
+
+    if (animation.dataset.animTriggerHook) {
+      triggerHook = animation.dataset.animTriggerHook;
+      triggerHook = parseFloat(triggerHook)
+    } else {
+      triggerHook = 0
+    }
+
+    return triggerHook
+  })();
+
+  const animOffset = (() => {
+    let offset;
+
+    if (animation.dataset.animOffset) {
+      offset = animation.dataset.animOffset;
+      offset = parseInt(offset)
+    } else {
+      offset = vh;
+    }
+
+    return offset
+  })();
+
+
+  lottieInstance.addEventListener('DOMLoaded', async (e) => {
+    /* Waits until the SVGs have loaded to get height of container - prevents breaking of non-fixed height containers. */
+    let parent = animation.parentElement;
+
+    /* Build ScrollMagic scene once all SVGs are loaded*/
+    new ScrollMagic.Scene({
+        /* Animation will be linked to the scroll progress of its container, starting when the top of container is halfway through viewport and ending when bottom of container is halfway through viewport */
+        triggerElement: parent,
+        triggerHook: animTriggerHook,
+        offset: animOffset,
+        duration: animDuration === "parentHeight" ? parent.offsetHeight : animDuration === "elemHeight" ? animation.offsetHeight : animDuration
+      })
+      .addTo(controller)
+      // .addIndicators() /* Debugging tool to see where an  d when animations occur */
+      .on("progress", function (e) {
+        const scrollFrame = e.progress * (lottieInstance.totalFrames - 1);
+        lottieInstance.goToAndStop(scrollFrame, true);
+      });
+  })
+})
---
+++ script.js
@@ -0,0 +1,141 @@
+$(document).ready(function(){
+   $('.lottie').css("opacity", 0);
+   $('.hero-bullet-points').css("opacity", 0);
+
+   $(window).scroll(function(){
+       var vh = $(window).height();
+       var scroll_position = $(window).scrollTop() - vh;
+       var fade_duration = 200;
+
+       var bullet_1 = {
+         fade_in_start: 0,
+         fade_out_end: 1500,
+         class_name: '._1'
+       }
+
+       var bullet_2 = {
+         fade_in_start: 1500,
+         fade_out_end: 4500,
+         class_name: '._2'
+       }
+
+       var bullet_3 = {
+         fade_in_start: 4500,
+         fade_out_end: 5500,
+         class_name: '._3'
+       }
+
+       var bullet_4 = {
+         fade_in_start: 5500,
+         fade_out_end: 7500,
+         class_name: '._4'
+       }
+       var bullet_5 = {
+         fade_in_start:7500,
+         fade_out_end: 9500,
+         class_name: '._5'
+       }
+       var bullet_6 = {
+         fade_in_start:9500,
+         fade_out_end: 11500,
+         class_name: '._6'
+       }
+       var bullet_7 = {
+         fade_in_start:11500,
+         fade_out_end: 13500,
+         class_name: '._7'
+       }
+       var bullet_8 = {
+         fade_in_start:13500,
+         fade_out_end: 15500,
+         class_name: '._8'
+       }
+       var bullet_9 = {
+         fade_in_start:15500,
+         fade_out_end: 17500,
+         class_name: '._9'
+       }
+
+       function change(bullet){
+         var fade_in_end = bullet["fade_in_start"] + fade_duration;
+         var fade_out_start = bullet["fade_out_end"] - fade_duration;
+
+         if (scroll_position >= bullet["fade_in_start"] && scroll_position <= fade_out_start){
+           $(bullet["class_name"]).css("opacity", (scroll_position- bullet["fade_in_start"])/ fade_duration);
+         } else if (scroll_position >= fade_out_start && scroll_position <= bullet["fade_out_end"]){
+           $(bullet["class_name"]).css("opacity", 1-(scroll_position - fade_out_start)/ fade_duration);
+         }
+         console.log("change", bullet["class_name"], scroll_position, $(bullet["class_name"]).css("opacity"));
+       }
+
+       function set_to_zero(bullet){
+         $(bullet["class_name"]).css("opacity", 0);
+         console.log("set to 0", bullet["class_name"], scroll_position);
+       }
+
+       if (scroll_position >= bullet_1["fade_in_start"] && scroll_position <= bullet_1["fade_out_end"])
+       {
+         change(bullet_1);
+       } else {
+         set_to_zero(bullet_1);
+       }
+
+       if (scroll_position >= bullet_2["fade_in_start"] && scroll_position <= bullet_2["fade_out_end"])
+       {
+         change(bullet_2);
+       } else {
+         set_to_zero(bullet_2);
+       }
+
+       if (scroll_position >= bullet_3["fade_in_start"] && scroll_position <= bullet_3["fade_out_end"])
+       {
+         change(bullet_3);
+       } else {
+         set_to_zero(bullet_3);
+       }
+
+       if (scroll_position >= bullet_4["fade_in_start"] && scroll_position <= bullet_4["fade_out_end"])
+       {
+         change(bullet_4);
+       } else {
+         set_to_zero(bullet_4);
+       }
+
+       if (scroll_position >= bullet_5["fade_in_start"] && scroll_position <= bullet_5["fade_out_end"])
+       {
+         change(bullet_5);
+       } else {
+         set_to_zero(bullet_5);
+       }
+
+       if (scroll_position >= bullet_6["fade_in_start"] && scroll_position <= bullet_6["fade_out_end"])
+       {
+         change(bullet_6);
+       } else {
+         set_to_zero(bullet_6);
+       }
+
+       if (scroll_position >= bullet_7["fade_in_start"] && scroll_position <= bullet_7["fade_out_end"])
+       {
+         change(bullet_7);
+       } else {
+         set_to_zero(bullet_7);
+       }
+
+       if (scroll_position >= bullet_8["fade_in_start"] && scroll_position <= bullet_8["fade_out_end"])
+       {
+         change(bullet_8);
+       } else {
+         set_to_zero(bullet_8);
+       }
+
+       if (scroll_position >= bullet_9["fade_in_start"] && scroll_position <= bullet_9["fade_out_end"])
+       {
+         change(bullet_9);
+       } else {
+         set_to_zero(bullet_9);
+       }
+
+
+   })
+})
